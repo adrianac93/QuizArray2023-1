f81053c3e783aabc3526d67a5093d8fc8e0692cf
Adriana <Adriana@DESKTOP-4FE0QIE>
2025-11-10 16:47:59 -0400

    Finish my quiz

---
+++ QuizArrays/QuizArrays.Logic/TwoDimensions.cs
@@ -0,0 +1,57 @@
+﻿namespace QuizArrays.Logic
+{
+    public class TwoDimensions
+    {
+        private int[,] _array;
+
+        public TwoDimensions(int m, int n)
+        {
+            M = m;
+            N = n;
+            _array = new int[M, N];
+        }
+
+        public int M { get; }
+
+        public int N { get; }
+
+        public override string ToString()
+        {
+            var output = string.Empty;
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    output += $"{_array[i, j]}\t";
+                }
+                output += "\n";
+            }
+            return output;
+        }
+
+        public OneDimension ToOneDimension()
+        {
+            var oneDimension = new OneDimension(M * N);
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    oneDimension.Add(_array[i, j]);
+                }
+            }
+            return oneDimension;
+        }
+
+        public void Fill()
+        {
+            var random = new Random();
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    _array[i, j] = random.Next(1, 100);
+                }
+            }
+        }
+    }
+}
